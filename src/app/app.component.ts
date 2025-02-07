635ded9182d9eee0af48c4f7448bc4ab55c840e2
williamjkc69 <williamjkc69@gmail.com>
2025-02-07 16:36:54 -0600

    section 2 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-project-app';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+  nameSelected = '';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+    // this.nameSelected = DUMMY_USERS.find((x) => x.id === id)?.name || '';
+  }
 }
---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../task.service';
+
+@Component({
+  selector: 'app-task',
+  imports: [CardComponent, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+
+  private taskService = inject(TasksService);
+
+  onCompleteTask() {
+    this.taskService.removeTask(this.task.id);
+  }
+}
